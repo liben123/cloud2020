ef2fe9017d085232bc09cd59f2b0802691840b0f
liben123 <liben970819@163.com>
2020-07-23 11:18:10 +0800

    添加服务

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.service;
+
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout();
+}
