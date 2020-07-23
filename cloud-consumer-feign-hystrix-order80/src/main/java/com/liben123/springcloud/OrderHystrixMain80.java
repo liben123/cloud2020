13c84cef77e1a7087f25ada98a1d10a72711e33b
liben123 <liben970819@163.com>
2020-07-23 16:18:03 +0800

    添加消费熔断降级机制

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/liben123/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,11 @@
+package com.liben123.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableHystrix
+public class OrderHystrixMain80 {
+}
