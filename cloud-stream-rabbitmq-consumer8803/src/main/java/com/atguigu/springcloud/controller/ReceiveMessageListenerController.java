20a4ec735bb4796361c4dac961179f32bc42ea9b
liben123 <liben970819@163.com>
2020-07-24 15:01:12 +0800

    格式化

--- cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/controller/ReceiveMessageListenerController.java
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/controller/ReceiveMessageListenerController.java
@@ -10,15 +10,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 @EnableBinding(Sink.class)
-public class ReceiveMessageListenerController
-{
+public class ReceiveMessageListenerController {
     @Value("${server.port}")
     private String serverPort;
 
 
     @StreamListener(Sink.INPUT)
-    public void input(Message<String> message)
-    {
-        System.out.println("消费者2号,----->接受到的消息: "+message.getPayload()+"\t  port: "+serverPort);
+    public void input(Message<String> message) {
+        System.out.println("消费者2号,----->接受到的消息: " + message.getPayload() + "\t  port: " + serverPort);
     }
 }
